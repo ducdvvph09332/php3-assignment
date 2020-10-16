66057465110e8a77454b261b2f2f7bb585fec5c6
Việt Đức <ducdvvph09332@fpt.edu.vn>
2020-10-16 14:52:26 +0700

    update delete category

--- resources/views/admin/categories/index.blade.php
+++ resources/views/admin/categories/index.blade.php
@@ -26,7 +26,7 @@
                                         <line x1="12" y1="8" x2="12" y2="16"></line>
                                         <line x1="8" y1="12" x2="16" y2="12"></line>
                                     </svg></a>
-                            </>
+                                </>
                             </th>
                         </tr>
                     </thead>
@@ -34,7 +34,11 @@
                         @foreach($categories as $item)
                         <tr>
                             <td>{{$item->name}}</td>
-                            <td>{{isset($item->category->name) ? $item->category->name:"No parent !!!"}}</td>
+                            @if(isset($item->category->name))
+                            <td>{{$item->category->name}}</td>
+                            @else
+                            <td class="text-danger">Null</td>
+                            @endif
                             <td>
                                 <a href="{{route('categories.show',$item->id)}}" class="btn btn-info mt-2">
                                     <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-eye">
