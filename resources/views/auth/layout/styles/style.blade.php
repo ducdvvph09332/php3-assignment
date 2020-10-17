8809275aa3e4fd03984ab81a80cfe8fb857b330e
Việt Đức <ducdvvph09332@fpt.edu.vn>
2020-10-17 10:33:18 +0700

    login

---
+++ resources/views/auth/layout/styles/style.blade.php
@@ -0,0 +1,11 @@
+    <link rel="icon" type="image/x-icon" href="{{asset('admin/assets/img/favicon.ico')}}"/>
+    <!-- BEGIN GLOBAL MANDATORY STYLES -->
+    <link href="https://fonts.googleapis.com/css?family=Nunito:400,600,700" rel="stylesheet">
+    <link href="{{asset('admin/bootstrap/css/bootstrap.min.css')}}" rel="stylesheet" type="text/css" />
+    <link href="{{asset('admin/assets/css/plugins.css')}}" rel="stylesheet" type="text/css" />
+    <link href="{{asset('admin/assets/css/authentication/form-1.css')}}" rel="stylesheet" type="text/css" />
+    <!-- END GLOBAL MANDATORY STYLES -->
+    <link rel="stylesheet" type="text/css" href="{{asset('admin/assets/css/forms/theme-checkbox-radio.css')}}">
+    <link rel="stylesheet" type="text/css" href="{{asset('admin/assets/css/forms/switches.css')}}">
+
+    
