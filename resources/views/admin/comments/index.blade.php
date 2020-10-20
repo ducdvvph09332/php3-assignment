7b6b7b2a8db24b79ad9d4855b48a4ae1efb4a247
Việt Đức <ducdvvph09332@fpt.edu.vn>
2020-10-20 11:20:12 +0700

    delete with sweetalert confirm

--- resources/views/admin/comments/index.blade.php
+++ resources/views/admin/comments/index.blade.php
@@ -20,13 +20,6 @@
                             <th>Comment By</th>
                             <th>In Product</th>
                             <th>Content</th>
-                            <th>Action
-                                <a href="" class="btn btn-primary"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-plus-square">
-                                        <rect x="3" y="3" width="18" height="18" rx="2" ry="2"></rect>
-                                        <line x1="12" y1="8" x2="12" y2="16"></line>
-                                        <line x1="8" y1="12" x2="16" y2="12"></line>
-                                    </svg></a>
-                            </th>
 
                         </tr>
                     </thead>
@@ -36,31 +29,6 @@
                             <td>{{$item->user->first_name}} {{$item->user->last_name}}</td>
                             <td>{{$item->product->name}}</td>
                             <td>{{$item->content}}</td>
-                            <td>
-                                <a href="{{route('comments.show',$item->id)}}" class="btn btn-info mt-2">
-                                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-eye">
-                                        <path d="M1 12s4-8 11-8 11 8 11 8-4 8-11 8-11-8-11-8z"></path>
-                                        <circle cx="12" cy="12" r="3"></circle>
-                                    </svg>
-                                </a>
-                                <a href="{{route('comments.edit',$item->id)}}" class="btn btn-success mt-2">
-                                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-edit-2">
-                                        <path d="M17 3a2.828 2.828 0 1 1 4 4L7.5 20.5 2 22l1.5-5.5L17 3z"></path>
-                                    </svg>
-                                </a>
-                                <form action="{{route('comments.destroy', $item->id)}}" method="POST" class="mt-2">
-                                    @csrf
-                                    <input type="hidden" name="_method" value="DELETE" id="">
-                                    <button type="submit" class="btn btn-danger">
-                                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-trash-2 icon">
-                                            <polyline points="3 6 5 6 21 6"></polyline>
-                                            <path d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6m3 0V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2"></path>
-                                            <line x1="10" y1="11" x2="10" y2="17"></line>
-                                            <line x1="14" y1="11" x2="14" y2="17"></line>
-                                        </svg>
-                                    </button>
-                                </form>
-                            </td>
                         </tr>
                         @endforeach
                     </tbody>
